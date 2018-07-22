36dd10d4434876d04b04a7c2baf582da11a9b4d4
idchlife <idchlife@gmail.com>
2018-07-22 23:33:34 +0300

    message_queue in constructor = no custom events

--- app.py
+++ app.py
@@ -1,24 +1,52 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send, emit
+from eventlet import monkey_patch
+
+monkey_patch()
 
 
 app = Flask(__name__)
 
-socketio = SocketIO(logger=True)
+"""
+UNCOMMENT ONE OF THE SCENARIOS CODES AND COMMENT OTHERS TO TEST
+"""
+
+"""SCENARIO 1
+message_queue in constructor and in .init_app
+
+Client is not receiving custom events
+"""
+#socketio = SocketIO(message_queue="redis://")
+#socketio.init_app(app, message_queue="redis://")
+
+"""SCENARIO 2
+message_queue in constructor only
+
+Client is not receiving custom events
+"""
+# socketio = SocketIO(message_queue="redis://")
+# socketio.init_app(app)
+
+"""SCENARIO 3
+message_queue in init_app
+
+Client is receiving events as it should
+"""
+socketio = SocketIO()
+socketio.init_app(app, message_queue="redis://")
 
 @app.route("/")
 def test_socket():
   return render_template("test-socket.html")
 
 
+@socketio.on("custom_ping")
+def ping():
+  print("PINGED!")
+  emit("custom_pong", {'data': 2323})
+
 @socketio.on("connect")
 def on_connect():
   print("Someones connected! Sending bacj")
-  emit("custom", { 'msg': "HELLO" })
-
-@socketio.on("ping")
-def ping():
-  print("PINGED!")
-  emit("custom", { 'msg': "PONG" })
-
-socketio.init_app(app)
+  emit("custom", {'data': 2323})
+  socketio.emit("custom", "well")
